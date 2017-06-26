7f267655b288d27a06e41b94f1c9bb85d11f669a
Nick Onley <nick.onley@aol.com>
2017-06-26 16:10:08 -0400

    Fixed the issues ith writing to firebase, but still need to debug the reading from firebase functionality. The DiningHall component reads from firebase if you hard reload the page but not when switching back and forth between dining hall pages.

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCxCiyNmqBqvzmdV4bLCdoyGCequElpY1U",
+  authDomain: "plater8-ba3b1.firebaseapp.com",
+  databaseURL: "https://plater8-ba3b1.firebaseio.com",
+  projectId: "plater8-ba3b1",
+  storageBucket: "plater8-ba3b1.appspot.com",
+  messagingSenderId: "857474503510"
+};
+firebase.initializeApp(config);
+
+//var database = firebase.database();
+var firebaseRunk = firebase.database().ref("Runk");
+var firebaseOHill = firebase.database().ref("Ohill");
+var firebaseNewcomb = firebase.database().ref("Newcomb");
+
+export { firebaseRunk, firebaseOHill, firebaseNewcomb };
